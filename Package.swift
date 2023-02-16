a7e4c5e45d40a4532874bf1c73220a8e752b5d42
Bofei Zhu <bofei.zhu@healthtap.com>
2023-02-16 20:58:20 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/109.0.1/WebRTC-M109.0.1.xcframework.zip",
+            url: "https://github.com/bofeizhu/WebRTC/releases/download/109.0.1/WebRTC-M109.0.1.xcframework.zip",
             checksum: "28f5fa694cc9ccc36eb5b7ac19305e1fbf0ade03eabfd02cbc0386cda693c9f1"
         ),
     ]
